1a2c4b90afbeb9e5a97682731cd5bfb6b05e1c76
datweb07 <dat82770@gmail.com>
2025-11-09 21:04:19 +0700

    update Diagram, delete Bill, BillData

--- ComponentListData/ComboProductList.cs
+++ ComponentListData/ComboProductList.cs
@@ -1,14 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace OOP_finalProject
 {
     [Serializable]
-    public class ComboProductList 
+    public class ComboProductList : ISerializable
     {
         //private List<ComboProduct> comboProducts = new List<ComboProduct>();
+        public void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
